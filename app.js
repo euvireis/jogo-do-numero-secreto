5fa7299c302245a57e1b10e1f7d960b18e8d92e8
Victória Reis <victoriagr@gmail.com>
2025-08-05 21:59:53 +0100

    emoji festa retirado

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ function verificarChute() {
     if (chute==numeroSecreto) {
         exibirTextoNaTela('h1', 'Acertou!');
         let palavraTentativa = tentativas > 1 ? 'tentativas': 'tentativa';
-        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}! 🎉`;
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
         document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
